1cd7ab34d10b68cf420744ef21af96c9c149e4c9
JyGein <52318346+JyGein@users.noreply.github.com>
2024-04-11 08:26:50 -0400

    slider work

--- catalog assets/CardsParent.gd
+++ catalog assets/CardsParent.gd
@@ -25,6 +25,7 @@ func _ready():
 				card.texture = load("res://Cards/{0}".format([file_name]))
 				var scale_factor = CARD_WIDTH/card.texture.get_size().x
 				card.scale = Vector2(scale_factor, scale_factor)
+				@warning_ignore("integer_division")
 				card.position = Vector2((CATALOG_WIDTH*0.1)+(count%4)*(CARD_WIDTH+SPACING), (CATALOG_WIDTH*0.1)+(count/4)*(CARD_HEIGHT+SPACING)) #warning-ignore:integer_division
 				count+=1
 			file_name = dir.get_next()
@@ -32,12 +33,12 @@ func _ready():
 var last_drag_time = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(position.y)
-	print(-((count/4)*(CARD_HEIGHT+SPACING)-SPACING))
 	if position.y > 0 && Time.get_ticks_msec() > (last_drag_time+100):
 		position.y-=(position.y+10)*(1.0/10.0)
-	if position.y < -((count/4)*(CARD_HEIGHT+SPACING)-SPACING) && Time.get_ticks_msec() > (last_drag_time+100):
-		position.y-=(position.y+((count/4)*(CARD_HEIGHT+SPACING)-SPACING)-10)*(1.0/10.0)
+	@warning_ignore("integer_division")
+	var CATALOG_HEIGHT = ((count/4)*(CARD_HEIGHT+SPACING)-SPACING)
+	if position.y < -CATALOG_HEIGHT && Time.get_ticks_msec() > (last_drag_time+100):
+		position.y-=(position.y+CATALOG_HEIGHT-10)*(1.0/10.0)
 
 func _on_texture_button_gui_input(event):
 	if event is InputEventMouseMotion:
